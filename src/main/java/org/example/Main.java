d3cc5e02f127ee669afa15604965eb984098297a
akash garg <MY_NAME@example.com>
2022-12-28 13:22:06 +0530

    update pom

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
